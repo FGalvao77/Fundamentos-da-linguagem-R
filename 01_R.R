d67254a4551e2481d8ce363fdab758a8566882bc
Fernando Galvão <fernando.galvao77@outlook.com>
2021-05-02 00:38:27 -0300

    conhecendo a linguagem R e sua sintaxe

---
+++ 01_R.R
@@ -0,0 +1,18 @@
+# R
+
+# R é um software estatístico/linguagem de programação
+# muito utilizado no campo acadêmico. Domina-lo é requesito
+# essencial para Ciência de Dados e áreas correlatas,
+# através da linguagem podemos manipular, visualizar dados e,
+# criar modelos utilizando técnicas de Machine Learning,
+# O R também fornce recursos para construir aplicações com
+# o R Markdown (sites, livros online, formulários...).
+
+# Vantagens da utilização do R:
+  # - gratuito (open-source)
+  # - comunidade cada vez mmais ativa e em amplo crescimento
+  # - milhares de pacotes para diversas aplicações
+  # - multi-plataforma, compátivel com Windows, Mac, Linux,..
+
+# Vamos para nosso primeiro código!
+print('Hello, World!')
